c46b74f0a4a38f82d3908b5d87c2ad741859447e
SimonB87 <buryans@gmail.com>
2021-03-22 23:41:18 +0100

    JS - default arguments

---
+++ JS/13_JSnewFeatures/script1.js
@@ -0,0 +1,14 @@
+// # Default Params
+
+function rollDie(numSides) {
+  return Math.floor(Math.random() * numSides) + 1;
+}
+
+// - in the rollDie we have to provide a parameter
+
+
+function rollBetterDie(numSides = 6) {
+  return Math.floor(Math.random() * numSides) + 1;
+}
+
+// - in the rollBetterDie the defauůt value is 6.
